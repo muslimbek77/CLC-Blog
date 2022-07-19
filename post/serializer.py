26d7a3f63cbb55d45f85b70c1f6df0657f6c24fc
Javohir Elmurodov <elmurodovjavohir@gmail.com>
2022-07-19 17:00:05 +0500

    feat: author detail with post detail

--- post/serializer.py
+++ post/serializer.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
 from post.models import Post
-
+from author.serializer import AuthorDetailSerializer
 
 class PostSerializer(serializers.ModelSerializer):
-
+    author = AuthorDetailSerializer()
     class Meta:
         model = Post
         fields = "__all__"
